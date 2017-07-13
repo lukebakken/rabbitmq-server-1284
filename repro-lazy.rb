997c18ae57410ce0511dd5ea01a19fe5f6c2d951
Luke Bakken <luke@bakken.io>
2017-07-13 13:05:00 -0700

    improvement

--- repro-lazy.rb
+++ repro-lazy.rb
@@ -1,12 +1,14 @@
 require 'bunny'
 
-msg_size = Integer(ARGV[0])
+pub_count = Integer(ARGV[0])
+
+msg_size = Integer(ARGV[1])
 msg = Random.new.bytes(msg_size)
 
 b = Bunny.new.start
 ch = b.create_channel
 q = ch.queue("lazy_#{msg_size}", durable: true)
 puts q.name
-4096.times do
+pub_count.times do
   q.publish msg
 end
